5268aa5ef76f0274a2651fdc9610ca8dca00b68f
Yuang <fyou@vip.qq.com>
2018-03-20 22:24:50 +0800

    git tracks changes

---
+++ src/test/java/info/iest/doto_list/AppTest.java
@@ -0,0 +1,16 @@
+
+package info.iest.doto_list;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+
+@CucumberOptions(plugin={"pretty"}, features={"src/test/features"})
+
+ 
+
+public class AppTest {
+
+     
+
+}
